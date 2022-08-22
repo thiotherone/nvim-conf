7e4169092ebb144de954f54139a4d32926571ab4
pescudeiro <pescudeiro@itba.edu.ar>
2022-08-22 17:52:21 -0300

    Updates

--- lua/pato/treesitter.lua
+++ lua/pato/treesitter.lua
@@ -8,10 +8,23 @@ configs.setup({
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
+		disable = { "" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "python", "css" } },
+	indent = { enable = true, disable = {} },
+	autopairs = { enable = true }, -- autopairs integration
+    context_commentstring = { -- Comment.nvim integration
+        enable = true,
+        enable_autocmd = false,
+    },
+    rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        -- colors = {}, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+    },
 })
